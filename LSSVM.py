7db11051c49325a7282adb441a38acfaab0ab76a
Ferwardo <ferresax@hotmail.com>
2022-12-08 12:59:59 +0100

    Added the normal and abnormal generated dataset

--- LSSVM.py
+++ LSSVM.py
@@ -2,7 +2,82 @@ import tensorflow as tf
 
 
 class LSSVM:
-    def __init__(self):
-        self.Beta = tf.Variable([])
-        self.Omega = tf.Variable([])
-        self.P = tf.Variable([])
+    def __init__(self, Beta=tf.Variable([]), Omega=tf.Variable([]), P_inv=tf.Variable([]), X_pv=tf.Variable([]),
+                 zeta=tf.Variable([]), config=None):
+        # Initialise parameters of the model
+        self.Beta = Beta
+        self.Omega = Omega
+        self.Omega_inv = tf.inv(Omega)
+        self.P_inv = P_inv
+        self.X_pv = X_pv
+        self.zeta = zeta
+
+        if config is None:
+            config = {
+                "K": 2,
+                "Ninit": 1,
+                "PVinit": 1,
+                "M": 1500,
+                "C": 10,
+                "sigma": 20,
+                "threshold_type": 'fxd',
+                "threshold_minimum": 0.05,
+            }
+        self.config = config
+
+    def compute(self, X_init, Y_init, X_pv=None, C=None):
+        """
+        Compute the model parameters from scratch
+        :param X_init: The observations used to initialise the model
+        :param X_pv: The prototype vectors used to initialise the model
+        """
+
+        # Compute Omega_mm and its inverse
+        if X_pv is None:
+            X_pv = self.X_pv
+
+        self.Omega = self.__gen_kernel_matrix(X_pv, X_pv, self.config["sigma"])
+        self.Omega_inv = tf.inv(self.Omega)
+
+        # Compute Omega_tm for the rest of the calculations
+        Omega_tm = self.__gen_kernel_matrix(X_init, X_pv, self.config["sigma"])
+
+        # Compute P_inv with ((Omega_tm'*Omega_tm)+C*Omega_mm)^-1
+        if C is None:
+            C = self.config["C"]
+
+        self.P_inv = tf.inv(
+            tf.matmul(Omega_tm, Omega_tm, transpose_a=True) + tf.scalar_mul(C, self.Omega))
+
+        # Compute Beta with P_inv . Omega_tm' . Y_init
+        self.Beta = tf.matmul(tf.matmul(self.P_inv, Omega_tm, transpose_b=True), Y_init)
+
+        # Compute the cost zeta
+        tf.norm(Y_init - tf.matmul(Omega_tm, self.Beta) ** 2 + tf.scalar_mul(C, tf.matmul(
+            tf.matmul(self.Beta, self.Omega, transpose_a=True), self.Beta)))
+
+    # Private helper functions
+    def __gen_kernel_matrix(self, X, X_t, sigma):
+        """
+        Computes the RBF kernel matrix between X and Xt given kernel bandwidth sigma
+        :param X: An N times D data matrix
+        :param X_t: An N_t times D data matrix
+        :param sigma: The kernel bandwidth
+        :return: The N times N_t kernel matrix.
+        """
+
+        size_x = X.shape
+        size_x_t = X_t.shape
+        Omega = tf.Variable(tf.zeros([size_x[0], size_x_t[0]]))
+
+        assert size_x[1] != size_x_t[1], "The matrices do not have the right size."
+
+        for n in range(0, size_x[0]):
+            for nt in range(0, size_x_t[0]):
+                # Compute the kernel value with the following formula: kerval=||X(n,:)-X_t(nt,:)||²
+                kerval = tf.norm(tf.subtract(X[n, :], X_t[nt, :])) ** 2
+
+                # Compute the current row for Omega
+                Omega[n, nt] = tf.exp(-kerval / sigma)
+
+        return Omega
--- dataset_gen.py
+++ dataset_gen.py
@@ -1,6 +1,8 @@
 import math
+import os
 
 import matplotlib.pyplot as plt
+import scipy.signal
 from scipy.fft import fft, fftfreq
 import numpy as np
 
@@ -11,8 +13,13 @@ def sine_wave(frequency=440.0, samplerate=88200, t=5000):
     return sinusoid
 
 
+VISUALISE_NORMAL = True
+VISUALISE_ABNORMAL = True
+
 for frequency in range(440, 4401, 440):
     samplerate = 44100
+
+    # Normal wave
     y = [j for j in sine_wave(frequency=frequency, samplerate=samplerate)]
     x = range(0, len(y))
 
@@ -20,18 +27,47 @@ for frequency in range(440, 4401, 440):
     values = fft(y)
     frequencies = fftfreq(len(x), 1 / samplerate)
 
-    # Show the frequency plot for each channel
-    plt.subplot(211)
-    plt.title(f"Frequency: {frequency}")
-    plt.plot(frequencies, np.abs(values))
-    plt.xlabel("Frequency (Hz)")
-    plt.ylabel("Value")
+    if VISUALISE_NORMAL:
+        # Show the frequency plot for each channel
+        plt.subplot(211)
+        plt.title(f"Frequency: {frequency}")
+        plt.plot(frequencies, np.abs(values))
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Value")
 
-    plt.subplot(212)
-    powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(y, Fs=samplerate)
-    plt.ylabel('Frequency (Hz)')
-    plt.xlabel('Time (s)')
-    plt.colorbar()
-    plt.show()
+        plt.subplot(212)
+        powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(y, Fs=samplerate)
+        plt.ylabel('Frequency (Hz)')
+        plt.xlabel('Time (s)')
+        plt.colorbar()
+        plt.show()
 
-    np.save(f"./dataset/sine/{frequency}", np.array([len(y), y]))
+        os.makedirs("./dataset/normal", exist_ok=True)
+        np.save(f"./dataset/normal/{frequency}", np.array([len(y), y]))
+
+    # Abnormal wave
+    t = np.linspace(0, 5, int(1000 * 5))
+    y2 = scipy.signal.chirp(t, frequency, 5, frequency + 880, method="quadratic")
+    x2 = range(0, len(y2))
+
+    # Do the FFT
+    values = fft(y2)
+    frequencies = fftfreq(len(x2), 1 / samplerate)
+
+    if VISUALISE_ABNORMAL:
+        # Show the frequency plot for each channel
+        plt.subplot(211)
+        plt.title(f"Frequency: {frequency}")
+        plt.plot(frequencies, np.abs(values))
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Value")
+
+        plt.subplot(212)
+        powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(y2, Fs=samplerate)
+        plt.ylabel('Frequency (Hz)')
+        plt.xlabel('Time (s)')
+        plt.colorbar()
+        plt.show()
+
+        os.makedirs("./dataset/abnormal", exist_ok=True)
+        np.save(f"./dataset/abnormal/{frequency}", np.array([len(y2), y2]))
